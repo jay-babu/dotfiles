e52187b8ae4c3d7ea5dd918d288fcd2d6bd5b87c
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-01-28 22:51:48 -0500

    add to shells

--- zsh/init.sh
+++ zsh/init.sh
@@ -5,6 +5,7 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 /home/linuxbrew/.linuxbrew/bin/brew install zsh
+echo "/home/linuxbrew/.linuxbrew/bin/zsh" | sudo tee -a /etc/shells
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
