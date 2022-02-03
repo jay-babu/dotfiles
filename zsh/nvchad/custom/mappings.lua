ea660f9c70127b9866a251cc784464a366c7bc6a
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-02-03 00:37:41 -0500

    adding TZ Zen mappings

--- zsh/nvchad/custom/mappings.lua
+++ zsh/nvchad/custom/mappings.lua
@@ -3,3 +3,8 @@ local map = require("core.utils").map
 -- bufferline
 map("n", "gb", ":BufferLinePick<CR>")
 
+-- truezen
+map("n", "<leader>ta", ":TZAtaraxis <CR>")
+map("n", "<leader>tm", ":TZMinimalist <CR>")
+map("n", "<leader>tf", ":TZFocus <CR>")
+
