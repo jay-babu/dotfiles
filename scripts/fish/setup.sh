a245dd60c2322c932dfeb6956b82dbf78d048f09
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-02-12 15:36:42 -0500

    Add tide to fish plugins and configure it

--- scripts/fish/setup.sh
+++ scripts/fish/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-fisher install jethrokuan/z IlanCosman/tide@v5 jhillyerd/plugin-git PatrickF1/fzf.fish edc/bass andreiborisov/sponge
+fisher install jethrokuan/z IlanCosman/tide@v5 jhillyerd/plugin-git PatrickF1/fzf.fish edc/bass andreiborisov/sponge IlanCosman/tide@v5
 
