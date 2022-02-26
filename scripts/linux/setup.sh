e20b3d2a3ecdf3bc0520dfc91f9dbc309965c3dc
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-02-25 19:48:21 -0500

    viola dot

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+chmod u+x ./linux/tools.sh
+./linux/tools.sh &
+
 brew tap ethereum/ethereum
 brew install zsh gcc node@16 exa gh openjdk@11 neovim fzf bat protobuf go gitui cheat solidity tmux fish fisher ripgrep stylua luarocks shellcheck lua-language-server
 
@@ -9,9 +12,6 @@ go install github.com/google/wire/cmd/wire@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
-
-chmod u+x ./tools.sh
-./tools.sh &
 
 git config --global user.email "36803168+jayp0521@users.noreply.github.com"
 git config --global user.name "Jay Patel"
