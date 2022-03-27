21aa2d06021e62bc117bb1efe5e7a393e707fff4
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-03-27 12:53:33 -0400

    remove scripts

--- scripts/linux/tools.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v brew &>/dev/null; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-(npm install -g npm@latest && npm install -g neovim bash-language-server vscode-langservers-extracted graphql-language-service-cli solidity-language-server typescript-language-server) &
-(pip3 install --upgrade pynvim pyright) &
