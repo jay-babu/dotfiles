5ead7ecf7de683f02de1afc0443a6978158020ef
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-04-28 16:22:02 -0400

    treesitter

--- nvim/.config/nvim/lua/user/plugins/init.lua
+++ nvim/.config/nvim/lua/user/plugins/init.lua
@@ -1,15 +1,18 @@
 return function(plugins)
-  local user_plugins = {
-    {
-      "catppuccin/nvim",
-      as = "catppuccin",
-      config = function ()
-        -- code
-        require("catppuccin").setup {}
-      end
-    },
-  }
+	local user_plugins = {
+		{
+			"catppuccin/nvim",
+			as = "catppuccin",
+			config = function()
+				-- code
+				require("catppuccin").setup({})
+			end,
+		},
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			after = "nvim-treesitter",
+		},
+	}
 
-  return vim.tbl_deep_extend("force", plugins, user_plugins)
+	return vim.tbl_deep_extend("force", plugins, user_plugins)
 end
-
---
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -0,0 +1,59 @@
+return {
+	ensure_installed = {
+		"bash",
+		"cpp",
+		"css",
+		"dockerfile",
+		"go",
+		"html",
+		"http",
+		"javascript",
+		"json",
+		"lua",
+		"make",
+		"markdown",
+		"python",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+	},
+	matchup = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ax"] = "@class.outer",
+				["ix"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]f"] = "@function.outer",
+				["]x"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]F"] = "@function.outer",
+				["]X"] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[f"] = "@function.outer",
+				["[x"] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[F"] = "@function.outer",
+				["[X"] = "@class.outer",
+			},
+		},
+		swap = {
+			enable = false,
+		},
+	},
+}
