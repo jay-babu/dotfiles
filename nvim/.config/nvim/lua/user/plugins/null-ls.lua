33752ca5ffcc1531712d8ad50a305d8c5865d2f6
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-05-14 11:45:16 -0400

    migrating null-ls to new format

---
+++ nvim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -0,0 +1,42 @@
+return function(config)
+	local null_ls = require("null-ls")
+	local b = null_ls.builtins
+	config.debug = false
+	config.sources = {
+		-- Lua
+		b.formatting.stylua.with({}),
+		b.diagnostics.luacheck,
+
+		-- Shell
+		b.formatting.shfmt,
+		b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#(c)]" }),
+		b.diagnostics.semgrep,
+
+		-- Docker
+		b.diagnostics.hadolint,
+
+		b.diagnostics.ansiblelint,
+
+		-- Golang
+		-- b.formatting.gofumpt,
+
+		-- JS
+		b.formatting.eslint_d,
+		b.diagnostics.eslint_d,
+		b.code_actions.eslint_d,
+
+		-- Protobuf
+		b.formatting.protolint,
+		b.diagnostics.protolint,
+	}
+	config.on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				desc = "Auto format before save",
+				pattern = "<buffer>",
+				callback = vim.lsp.buf.formatting_sync,
+			})
+		end
+	end
+	return config
+end
