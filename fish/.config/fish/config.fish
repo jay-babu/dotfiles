24d03d212109433eb3a041dadd136cc5f9435c91
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-05-23 20:26:09 -0400

    sponge delay of 10

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,17 +5,14 @@ fish_add_path (which brew)/../../opt/openjdk@11/bin
 fish_add_path (which brew)/../../opt/nvim/bin
 fish_add_path /home/linuxbrew/.linuxbrew/bin 
 fish_add_path (go env GOPATH)/bin
-# fish_add_path /home/linuxbrew/.linuxbrew/bin 
-# fish_add_path /home/linuxbrew/.linuxbrew/opt/node@16/bin
-# fish_add_path /home/linuxbrew/.linuxbrew/opt/openjdk@11/bin
-# fish_add_path /home/linuxbrew/.linuxbrew/opt/nvim/bin
-# fish_add_path "(go env GOPATH)"/bin
 
 set -Ux DONT_PROMPT_WSL_INSTALL true
 
 set -Ux EDITOR nvim
 
 set -Ux XDEB_PKGROOT ~/.config/xdeb
+
+set sponge_delay 10
 
 alias v="nvim"
 
