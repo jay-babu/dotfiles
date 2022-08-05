c1dd172099573015ef51ea56678054ff29707e90
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-08-05 14:57:12 -0400

    modern colorscheme setting

--- shell/.config/nvim/lua/user/colorscheme.lua
+++ shell/.config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,3 @@
-local colorscheme = "default_theme"
-local theme_installed, _ = pcall(require, "catppuccin")
-
-if theme_installed then
-	colorscheme = "catppuccin"
-end
+local colorscheme = "catppuccin"
 
 return colorscheme
