ff9893b2a779530fe436702406c6d36e997e2b82
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-08-20 22:34:40 -0400

    adding media files

--- shell/.config/nvim/lua/user/plugins/telescope.lua
+++ shell/.config/nvim/lua/user/plugins/telescope.lua
@@ -43,10 +43,44 @@ return function(default)
 				".git",
 				"node_modules",
 			},
+			preview = {
+				mime_hook = function(filepath, bufnr, opts)
+					local is_image = function(filepath)
+						local image_extensions = { "png", "jpg", "gif" } -- Supported image formats
+						local split_path = vim.split(filepath:lower(), ".", { plain = true })
+						local extension = split_path[#split_path]
+						return vim.tbl_contains(image_extensions, extension)
+					end
+					if is_image(filepath) then
+						local term = vim.api.nvim_open_term(bufnr, {})
+						local function send_output(_, data, _)
+							for _, d in ipairs(data) do
+								vim.api.nvim_chan_send(term, d .. "\r\n")
+							end
+						end
+						vim.fn.jobstart({
+							"catimg",
+							filepath, -- Terminal image viewer command
+						}, { on_stdout = send_output, stdout_buffered = true })
+					else
+						require("telescope.previewers.utils").set_preview_message(
+							bufnr,
+							opts.winid,
+							"Binary cannot be previewed"
+						)
+					end
+				end,
+			},
 		},
 		pickers = {
 			find_files = {
 				hidden = true,
+			},
+		},
+		extensions = {
+			media_files = {
+				filetypes = { "png", "jpg", "mp4", "webm", "pdf", "gif" },
+				find_cmd = "rg",
 			},
 		},
 	}
