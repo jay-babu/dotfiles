626c9e545c0b9739ccdd28b200744880978107f1
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-08-22 22:45:05 -0400

    modifying cinnamon.lua

--- shell/.config/nvim/lua/user/plugins/cinnamon.lua
+++ shell/.config/nvim/lua/user/plugins/cinnamon.lua
@@ -1,9 +1,10 @@
 return {
-	always_scroll = true,
+	always_scroll = false,
 	centered = true,
+	default_delay = 3,
 	extended_keymaps = true,
 	extra_keymaps = true,
-	scroll_limit = 75,
-	default_delay = 3,
+	horizontal_scroll = false,
 	max_length = 500,
+	scroll_limit = 75,
 }
