52f3655d5458ce1e1e8a66d3a57091e39b1b6b78
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-10-31 23:38:16 -0400

    fix bufferline bug and add groupings

--- shell/.config/nvim/lua/user/plugins/bufferline.lua
+++ shell/.config/nvim/lua/user/plugins/bufferline.lua
@@ -1,5 +1,40 @@
 return {
 	options = {
 		diagnostics = "nvim_lsp",
+		close_command = function(bufnum)
+			require("bufdelete").bufdelete(bufnum, true)
+		end,
+		color_icons = true,
+		groups = {
+			options = {
+				-- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+				toggle_hidden_on_enter = true,
+			},
+			items = {
+				{
+					name = "Tests", -- Mandatory
+					highlight = { underline = true, sp = "blue" }, -- Optional
+					priority = 2, -- determines where it will appear relative to other groups (Optional)
+					icon = "", -- Optional
+					matcher = function(buf) -- Mandatory
+						return buf.filename:match("%_test")
+							or buf.filename:match("%_spec")
+							or buf.filename:match("%Test")
+					end,
+				},
+				{
+					name = "Docs",
+					highlight = { undercurl = true },
+					icon = "",
+					auto_close = true, -- whether or not close this group if it doesn't contain the current buffer
+					matcher = function(buf)
+						return buf.filename:match("%.md") or buf.filename:match("%.txt")
+					end,
+					separator = { -- Optional
+						style = require("bufferline.groups").separator.tab,
+					},
+				},
+			},
+		},
 	},
 }
