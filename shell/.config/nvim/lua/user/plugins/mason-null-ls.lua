655276aaf76d820e97897e010cc886487f1cc5f4
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-11-29 20:28:36 -0500

    chore: opt for null-ls to be source of truth. mason-null-ls will auto install based on that

--- shell/.config/nvim/lua/user/plugins/mason-null-ls.lua
+++ shell/.config/nvim/lua/user/plugins/mason-null-ls.lua
@@ -1,15 +1,4 @@
 return {
 	automatic_installation = true,
-	automatic_setup = true,
-	ensure_installed = {
-		"black",
-		"eslint_d",
-		"flake8",
-		"hadolint",
-		"isort",
-		"protolint",
-		"shfmt",
-		"staticcheck",
-		"stylua",
-	},
+	automatic_setup = false,
 }
