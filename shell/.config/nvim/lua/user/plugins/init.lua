fbba3dab50d869935fd2d17500a066c0504e19a9
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-12-14 23:19:52 -0500

    feat(tmux): add vim-tmux-navigator

--- shell/.config/nvim/lua/user/plugins/init.lua
+++ shell/.config/nvim/lua/user/plugins/init.lua
@@ -377,4 +377,10 @@ return astronvim.user_plugin_opts("plugins.init", {
 			require("user.plugins.go")
 		end,
 	},
+	["christoomey/vim-tmux-navigator"] = {
+		opt = true,
+		setup = function()
+			table.insert(astronvim.file_plugins, "vim-tmux-navigator")
+		end,
+	},
 }, nil, "work")
