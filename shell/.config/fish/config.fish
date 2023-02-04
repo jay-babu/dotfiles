7fb33ee08c20a8cf1e45e7c763a51b8439838cec
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-02-04 15:34:46 -0500

    feat: aws autocompletion

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -44,6 +44,8 @@ function .....
   ../../../..
 end
 
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
