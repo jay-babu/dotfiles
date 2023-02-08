a45c2fe6d7736e0c2e6a1fb366d9ecab2b2e43a1
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-02-08 12:32:22 -0500

    making lazy extensible to work

--- shell/.config/nvim/lua/user/lazy.lua
+++ shell/.config/nvim/lua/user/lazy.lua
@@ -1,5 +1,8 @@
-return astronvim.user_opts("work.lazy", {
-	dev = {
-		path = "~/code",
-	},
-})
+return function(config)
+	config = vim.tbl_deep_extend("force", config, {
+		dev = {
+			path = "~/code",
+		},
+	})
+	return astronvim.user_opts("work.lazy", config)
+end
