c939d31d076cc71b02edd629421c532e9a698b8d
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-02-08 00:27:11 -0500

    chore: enabling textobjects.

--- shell/.config/nvim/lua/user/plugins/treesitter.lua
+++ shell/.config/nvim/lua/user/plugins/treesitter.lua
@@ -17,67 +17,62 @@ return {
 		},
 		opts = {
 			auto_install = vim.fn.executable("tree-sitter") == 1,
-			rainbow = {
-				enable = true,
-				extended_mode = true,
-				max_file_lines = nil,
-			},
 			matchup = {
 				enable = true,
 			},
-			-- textobjects = {
-			-- 	select = {
-			-- 		enable = true,
-			-- 		lookahead = true,
-			-- 		keymaps = {
-			-- 			["af"] = "@function.outer",
-			-- 			["if"] = "@function.inner",
-			-- 			["ax"] = "@class.outer",
-			-- 			["ix"] = "@class.inner",
-			-- 		},
-			-- 	},
-			-- 	move = {
-			-- 		enable = true,
-			-- 		set_jumps = true,
-			-- 		goto_next_start = {
-			-- 			["]f"] = "@function.outer",
-			-- 			["]x"] = "@class.outer",
-			-- 		},
-			-- 		goto_next_end = {
-			-- 			["]F"] = "@function.outer",
-			-- 			["]X"] = "@class.outer",
-			-- 		},
-			-- 		goto_previous_start = {
-			-- 			["[f"] = "@function.outer",
-			-- 			["[x"] = "@class.outer",
-			-- 		},
-			-- 		goto_previous_end = {
-			-- 			["[F"] = "@function.outer",
-			-- 			["[X"] = "@class.outer",
-			-- 		},
-			-- 	},
-			-- 	swap = {
-			-- 		enable = true,
-			-- 		swap_next = {
-			-- 			[">B"] = { query = "@block.outer", desc = "Swap next block" },
-			-- 			[">F"] = { query = "@function.outer", desc = "Swap next function" },
-			-- 			[">P"] = { query = "@parameter.inner", desc = "Swap next parameter" },
-			-- 		},
-			-- 		swap_previous = {
-			-- 			["<B"] = { query = "@block.outer", desc = "Swap previous block" },
-			-- 			["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-			-- 			["<P"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
-			-- 		},
-			-- 	},
-			-- 	lsp_interop = {
-			-- 		enable = true,
-			-- 		border = "single",
-			-- 		peek_definition_code = {
-			-- 			["<leader>lp"] = { query = "@function.outer", desc = "Peek function definition" },
-			-- 			["<leader>lP"] = { query = "@class.outer", desc = "Peek class definition" },
-			-- 		},
-			-- 	},
-			-- },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ax"] = "@class.outer",
+						["ix"] = "@class.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]x"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]X"] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[x"] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[X"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						[">B"] = { query = "@block.outer", desc = "Swap next block" },
+						[">F"] = { query = "@function.outer", desc = "Swap next function" },
+						[">P"] = { query = "@parameter.inner", desc = "Swap next parameter" },
+					},
+					swap_previous = {
+						["<B"] = { query = "@block.outer", desc = "Swap previous block" },
+						["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+						["<P"] = { query = "@parameter.inner", desc = "Swap previous parameter" },
+					},
+				},
+				lsp_interop = {
+					enable = true,
+					border = "single",
+					peek_definition_code = {
+						["<leader>lp"] = { query = "@function.outer", desc = "Peek function definition" },
+						["<leader>lP"] = { query = "@class.outer", desc = "Peek class definition" },
+					},
+				},
+			},
 		},
 	},
 	{
