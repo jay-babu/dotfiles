81401bed734e7880c47ce86f07051ff6047977a1
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-02-17 19:59:23 -0500

    feat: add startup time to alpha

--- shell/.config/nvim/lua/user/plugins/alpha.lua
+++ shell/.config/nvim/lua/user/plugins/alpha.lua
@@ -10,6 +10,21 @@ return {
 		end
 
 		dashboard.section.header.val = vim.fn.split(result, "\n")
+
+		vim.api.nvim_create_autocmd("UIEnter", {
+			-- pattern = "UIEnter",
+			callback = function()
+				local stats = require("lazy").stats()
+				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+				dashboard.section.footer.val = {
+					" ",
+					" ",
+					" ",
+					"AstroNvim loaded " .. require("lazy").stats().count .. " plugins  in " .. ms .. " ms",
+				}
+			end,
+		})
+
 		return dashboard
 	end,
 }
