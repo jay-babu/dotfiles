63f6f661ad2cd0a902f86cf9cbd44f09786b65db
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-02-18 21:59:15 -0500

    feat: add one-small-step-for-vimkind

--- shell/.config/nvim/lua/user/plugins/dap.lua
+++ shell/.config/nvim/lua/user/plugins/dap.lua
@@ -2,6 +2,7 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		{ "theHamsta/nvim-dap-virtual-text", config = true },
+		{ "jbyuki/one-small-step-for-vimkind" },
 	},
 	config = function(_, _)
 		vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticWarn" })
@@ -16,5 +17,20 @@ return {
 		sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
 		sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
 		sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+
+		local dap = require("dap")
+		dap.configurations.lua = {
+
+			{
+
+				type = "nlua",
+				request = "attach",
+				name = "Attach to running Neovim instance",
+			},
+		}
+
+		dap.adapters.nlua = function(callback, config)
+			callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+		end
 	end,
 }
