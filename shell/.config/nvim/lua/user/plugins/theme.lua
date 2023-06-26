01cfb885af09e78f83c2e3df15db94b8f412f1ba
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-06-26 11:55:58 -0400

    feat: add more themes and some colorscheme exclusions

--- shell/.config/nvim/lua/user/plugins/theme.lua
+++ shell/.config/nvim/lua/user/plugins/theme.lua
@@ -6,12 +6,16 @@ return {
 		},
 	},
 	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.colorscheme.monokai-pro-nvim", opts = { transparent_background = true } },
+	{ import = "astrocommunity.colorscheme.nightfox-nvim", opts = { options = { transparent = true } } },
 	{
 		"jay-babu/colorscheme-randomizer.nvim",
 		dependencies = {
 			{ "catppuccin/nvim", name = "catppuccin" },
 			{ "folke/tokyonight.nvim" },
 			{ "AstroNvim/astrotheme" },
+			{ "loctvl842/monokai-pro.nvim", opts = { transparent_background = true } },
+			{ "EdenEast/nightfox.nvim", opts = { options = { transparent = true } } },
 		},
 		opts = {
 			apply_scheme = true,
@@ -23,6 +27,13 @@ return {
 			-- colorschemes = {
 			-- 	"catppuccin-frappe",
 			-- },
+			exclude_colorschemes = {
+				"tokyonight-day",
+				"astrolight",
+				"catppuccin-latte",
+				"dawnfox",
+				"dayfox",
+			},
 		},
 		event = "User AstroFile",
 	},
