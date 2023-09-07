46fe3c592b0bb028bc31b8f7b3f3ef66dbb6db91
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-09-07 18:36:47 -0400

    feat: remove yanker

--- shell/.config/nvim/lua/user/options.lua
+++ shell/.config/nvim/lua/user/options.lua
@@ -11,22 +11,4 @@ local M = {
 	},
 }
 
-if vim.fn.has("wsl") == 1 then
-	M = require("astronvim.utils").extend_tbl(M, {
-		g = {
-			clipboard = {
-				name = "win32yank-wsl",
-				copy = {
-					["+"] = "win32yank.exe -i --crlf",
-					["*"] = "win32yank.exe -i --crlf",
-				},
-				paste = {
-					["+"] = "win32yank.exe -o --lf",
-					["*"] = "win32yank.exe -o --lf",
-				},
-				cache_enabled = 0,
-			},
-		},
-	})
-end
 return astronvim.user_opts("work.options", M)
