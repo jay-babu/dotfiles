9d80e3060d8433e65ae3faf2f174e7775ef8d802
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-09-21 16:50:59 -0400

    feat: options.lua add clipboard in wsl

--- shell/.config/nvim/lua/user/options.lua
+++ shell/.config/nvim/lua/user/options.lua
@@ -11,4 +11,22 @@ local M = {
 	},
 }
 
+if vim.fn.has("wsl") == 1 then
+	M = require("astronvim.utils").extend_tbl(M, {
+		g = {
+			clipboard = {
+				name = "WslClipboard",
+				copy = {
+					["+"] = "clip.exe",
+					["*"] = "clip.exe",
+				},
+				paste = {
+					["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\r", ""))',
+					["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\r", ""))',
+				},
+				cache_enabled = 0,
+			},
+		},
+	})
+end
 return astronvim.user_opts("work.options", M)
