7e4af95a3ce46cc2be70a880ec6e348222506302
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-11-29 22:06:31 -0800

    feat(nvim): switch to python-ruff

--- shell/.config/nvim/lua/user/plugins/community.lua
+++ shell/.config/nvim/lua/user/plugins/community.lua
@@ -62,7 +62,7 @@ return {
 	{ import = "astrocommunity.pack.json" },
 	{ import = "astrocommunity.pack.lua" },
 	{ import = "astrocommunity.pack.markdown" },
-	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.python-ruff" },
 	{ import = "astrocommunity.pack.typescript" },
 	{ import = "astrocommunity.git.git-blame-nvim" },
 	{ import = "astrocommunity.pack.rust" },
