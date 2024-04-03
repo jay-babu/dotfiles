4d16863c4aa4c140047a6d7a0b6627e9e4ca6894
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2024-04-02 22:16:16 -0400

    feat(nvim): adding spring_profiles_active local

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,11 +1,3 @@
-# set brew_location (which brew)
-# fish_add_path $brew_location/..
-# fish_add_path $brew_location/../../opt/node@16/bin
-# fish_add_path $brew_location/../../opt/openjdk@11/bin
-# fish_add_path $brew_location/../../opt/nvim/bin
-# fish_add_path /home/linuxbrew/.linuxbrew/bin
-# fish_add_path (go env GOPATH)/bin
-
 bass source ~/.profile
 
 set -x DONT_PROMPT_WSL_INSTALL true
@@ -14,13 +6,15 @@ set -x XDEB_PKGROOT ~/.config/xdeb
 
 set sponge_delay 100
 
+set -x spring_profiles_active local
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # set -gx FZF_DEFAULT_OPTS '--hidden --preview "bat --style=numbers --color=always --line-range :500 {}"'
 
     function cd
-        builtin cd $argv && exa -a -F --icons
+        builtin cd $argv && eza -a -F --icons
     end
 
     # Enable VI Mode
