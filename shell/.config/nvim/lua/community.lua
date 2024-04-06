796cf048254436f22efe006529cf553c5be59c37
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2024-04-06 11:07:59 -0400

    feat(nvim): update to main

--- shell/.config/nvim/lua/community.lua
+++ shell/.config/nvim/lua/community.lua
@@ -4,8 +4,7 @@
 
 ---@type LazySpec
 return {
-  -- TODO: Remove branch v4 on release
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  { "AstroNvim/astrocommunity", version = "^7" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.debugging.nvim-bqf" },
   { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
--- shell/.config/nvim/lua/lazy_setup.lua
+++ shell/.config/nvim/lua/lazy_setup.lua
@@ -1,13 +1,12 @@
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    branch = "v4", -- TODO: change `branch="v4"` to `version="^4"` on release
+    version = "^4",
     import = "astronvim.plugins",
     opts = { -- AstroNvim options must be set here with the `import` key
       mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
       maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
       icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
     },
   },
   { import = "community" },
